cd98d53f10fc421da6aa94b936d06b5e5ad9606a
Matthew James Briggs <matthew.james.briggs@gmail.com>
2019-04-14 01:28:26 -0400

    ci

--- .circleci/dockerfile-test-entrypoint.sh
+++ .circleci/dockerfile-test-entrypoint.sh
@@ -14,31 +14,15 @@ cat /etc/os-release
 sep
 cmake \
     -DEZXML_BUILD_TESTS=on \
-    -DEZXML_BUILD_CORE_TESTS=off \
     -DEZXML_BUILD_EXAMPLES=on \
     .
 
 sep
-make -j12
+make -j 6
 
 sep
-echo "running MxHide"
-./MxHide
-echo "success"
-
-sep
-echo "running MxWrite"
-./MxWrite
-echo "success"
-
-sep
-echo "running MxRead"
-./MxRead
-echo "success"
-
-sep
-echo "running MxTest"
-./MxTest
+echo "running tests"
+./ezxml_test
 echo "success"
 
 echo ""
