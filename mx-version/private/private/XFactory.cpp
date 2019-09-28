fa591eb77f39c0c4de6d0f28b548ec42f073940a
Matt Briggs <brigmatt>
2019-09-28 15:19:10 -0700

    temporarily stash the mx version

---
+++ mx-version/private/private/XFactory.cpp
@@ -0,0 +1,23 @@
+// MusicXML Class Library
+// Copyright (c) by Matthew James Briggs
+// Distributed under the MIT License
+
+#include "mx/xml/XFactory.h"
+#include "mx/xml/PugiDoc.h"
+#include "mx/xml/PugiElement.h"
+
+namespace ezxml
+{
+    namespace xml
+    {
+        XDocPtr XFactory::makeXDoc()
+        {
+            return XDocPtr{ new PugiDoc{} };
+        }
+        
+        XElementPtr XFactory::makeXElement()
+        {
+            return XElementPtr{ new PugiElement{} };
+        }
+    }
+}
